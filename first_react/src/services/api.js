b2f565d010a3c055ffe7bbeb9a430c1f02bff358
A.Hegarty <a.hegarty004@gmail.com>
2025-04-07 10:58:21 +0100

    removed

--- first_react/src/services/api.js
+++ first_react/src/services/api.js
@@ -1,5 +1,4 @@
-const API_KEY = "c9fbac6c8d909d4eeef5489a17d25e4f";
-const BASE_URL = "https://api.themoviedb.org/3";
+
 
 export const getPopMovies = async() =>{
     const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
